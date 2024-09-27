04a6da849ae681491cd3edd9bb70a33bcc32d5b1
jordanoboyle <jmcmcode@gmail.com>
2024-09-26 22:38:35 -0600

    dummy transactions, updatedValues to the DOM. Runs in init()

--- script.js
+++ script.js
@@ -27,11 +27,38 @@ function addTransactionToDOM(transaction) {
   item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
 
   item.innerHTML = `
-    ${transaction.text} <span> ${sign}${Math.abs(transaction.amount)} </span> <button class="delete-btn"> X </button>
+    ${transaction.text} <span> ${sign}${Math.abs(transaction.amount)} </span> <button class="delete-button"> X </button>
   `;
 
   list.appendChild(item);
 }
+
+//update balance Income and Expense
+function updateValues() {
+  const amounts = transactions.map(transaction => transaction.amount);
+
+  const total = amounts.reduce((accumulator, item) => (accumulator += item), 0).toFixed(2);
+
+  const income = amounts
+    .filter(item => item > 0)
+    .reduce((accumulator, item) => (accumulator += item), 0)
+    .toFixed(2);
+
+  const expense = (amounts
+    .filter(item => item < 0)
+    .reduce((accumulator, item) => (accumulator += item), 0)) * -1
+    .toFixed(2);    
+
+  balance.innerText = `$${total}`;
+  money_plus.innerText = `$${income}`;
+  money_minus.innerText = `$${expense}`;
+
+  console.log("the amounts", amounts);
+  console.log("TOTAL", total);
+  console.log('INCOME', income);
+  console.log('EXPENSE', expense);
+}
+
 
 //Init APP
 function init() {
@@ -39,6 +66,9 @@ function init() {
 
   transactions.forEach(addTransactionToDOM);
   //We do not want paranthesis here because if we do so then addTransactionToDOM is immediately invoked which is not desireable since this happens before 
+
+  updateValues();
 }
 
 init();
+
